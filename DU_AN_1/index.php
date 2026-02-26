745c26562a88b7c5c13aeea400d3acc9ae2bcac9
Cuongcuto522 <sccuong5222@gmail.com>
2026-02-26 21:33:18 +0700

    Add files via upload

--- DU_AN_1/index.php
+++ DU_AN_1/index.php
@@ -1,37 +1,6 @@
 <?php
 session_start();
-require_once "controllers/ProductController.php";
-require_once "controllers/CategoryController.php";
-$url = isset($_GET['action']) ? $_GET['action'] : "/";
-$productController = new ProductController();
-$categoryController = new CategoryController();
-switch($url){
-    case '/':
-        $productController->listProduct();
-        break;
-    case 'add-product':
-        $productController->addProduct();
-        break;
-    case 'edit-product':
-        $productController->editProduct();
-        break;
-    case 'delete-product':
-        $productController->deleteProduct();
-        break;
-    case 'list-category':
-        $categoryController->listCategory();
-        break;
-    case 'edit-category':
-        $categoryController->editCategory();
-        break;
-    case 'add-category':
-        $categoryController->addCategory();
-        break;
-    case 'delete-category':
-        $categoryController->deleteCategory();
-        break;
-    default:
-        echo "trang ko ton tai";
-        break;
-}
+require_once "env.php";
+require_once "vendor/autoload.php";
+require_once "common/route.php";
 ?>
